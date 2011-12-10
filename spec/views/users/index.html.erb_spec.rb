1a7aa9fc809e3fc7d4e6f5668df22e00155cbca5
Kyle Partridge <partkyle@gmail.com>
2011-12-10 14:01:07 -0800

    added user

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "users/index.html.erb" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :email => "Email",
+        :token => "Token"
+      ),
+      stub_model(User,
+        :email => "Email",
+        :token => "Token"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Token".to_s, :count => 2
+  end
+end
