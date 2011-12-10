44387ba0357f253b8bb5053f09516713ba95a71f
Kyle Partridge <partkyle@gmail.com>
2011-12-10 13:45:30 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xfit::Application.config.secret_token = 'e198c1d5d53661dbccc6efc6aa8c9425d6dbc63e2e340e97723b3ce978a909de9e83630141801cc8d0252f9995aeec912c42985b39a871e1e084c6b6279fe810'
