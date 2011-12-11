b0a5df6a67b0378ee9efbca7c5027031f657c6a6
David Tomberlin <david.tomberlin@sendgrid.com>
2011-12-10 18:40:20 -0800

    so much jq stuff

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,5 +2,11 @@
 
 $.jQTouch({
     icon: 'jqtouch.png',
-    statusBar: 'black-translucent'
+    statusBar: 'black-translucent',
+    formSelector: false
 });
+
+$('a').live('click', function(e){
+	e.preventDefault();
+	window.location.href = $(this).attr('href');
+})
